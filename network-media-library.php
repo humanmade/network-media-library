7608ce6a19b951f566edec31e1ece9da7bed5a24
Joi Glifberg <joi.glifberg@gmail.com>
2019-10-03 08:44:38 +0200

    Fix bug with image and file fields in repeatable fields returning the previous image or file field data due to filter order.

--- network-media-library.php
+++ network-media-library.php
@@ -414,7 +414,7 @@ class ACF_Value_Filter {
 	 *
 	 * @var mixed Field value.
 	 */
-	protected $value = null;
+	protected $value = [];
 
 	/**
 	 * Sets up the necessary action and filter callbacks.
@@ -457,7 +457,7 @@ class ACF_Value_Filter {
 			restore_current_blog();
 		}
 
-		$this->value = $image;
+		$this->value[ $field['name'] ] = $image;
 
 		return $image;
 	}
@@ -471,7 +471,7 @@ class ACF_Value_Filter {
 	 * @return mixed The updated value.
 	 */
 	public function filter_acf_attachment_format_value( $value, $post_id, array $field ) {
-		return $this->value;
+		return $this->value[ $field['name'] ];
 	}
 }
 
